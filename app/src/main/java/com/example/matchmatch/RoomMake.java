2f5c91a303ec53f4378c3ba04715ebbba20c48b7
YongGyunKwon <ygkwon50@gmail.com>
2020-11-08 04:24:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/matchmatch/RoomMake.java
@@ -0,0 +1,43 @@
+package com.example.matchmatch;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+//import androidx.preference.PreferenceFrageementCompat;
+
+
+public class RoomMake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addgame);
+
+
+
+
+//        Button submit=(Button)findViewById(R.id.submit);
+//
+//        submit.setOnClickListener(new Button.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                EditText room_subject1= (EditText)findViewById(R.id.room_subject);
+//                String room_sub=room_subject1.getText().toString();
+//                System.out.print(room_sub);
+//
+//            }
+//        }) ;
+
+    }
+
+//    public static class SettingsFragment extends PreferenceFragmentCompat {
+//        @Override
+//        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+//            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+//        }
+//    }
+}
