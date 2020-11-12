a5b5a32807e90fba68202481855ed06a931a5dc2
zzz4774 <zzz4774@naver.com>
2020-11-12 18:57:48 +0900

    ui변경

--- app/src/main/java/com/example/matchmatch/Datainfo.java
+++ app/src/main/java/com/example/matchmatch/Datainfo.java
@@ -7,18 +7,20 @@ public class Datainfo {
     String md;// 월,일
     String hm; //시간,분
     String state; //장소
+    String people;
 
     public Datainfo(){
 
     }
 
-    public Datainfo(String titletext, String md, String hm, String state) {
+    public Datainfo(String titletext, String md, String hm, String state, String people) {
         // TODO Auto-generated constructor stub
         //생성자함수로 전달받은 Member의 정보를 멤버변수에 저장
         this.titletext = titletext;
         this.md = md;
         this.hm = hm;
         this.state = state;
+        this. people = people;
     }
 
     public void setTitletext(String titletext) {
@@ -33,6 +35,9 @@ public class Datainfo {
     public void setState(String state) {
         this.state = state;
     }
+    public void setPeople(String people) {
+        this.people = people;
+    }
 
     public String getTitletext()
     {
@@ -46,6 +51,9 @@ public class Datainfo {
     }
     public String getState() {
         return state;
+    }
+    public String getPeople() {
+        return people;
     }
 
 
--- app/src/main/java/com/example/matchmatch/SoccerlistActivity.java
+++ app/src/main/java/com/example/matchmatch/SoccerlistActivity.java
@@ -59,9 +59,9 @@ public class SoccerlistActivity extends ListActivity {
 
         //dapter = new DatainfoAdapter();
 
-        datas.add(new Datainfo("재밌게 축구할 사람~", "2020-11-18", "20:30", "충북 청주시 개신동 충북대학교"));
-        datas.add(new Datainfo("축구 고수들만 모이세요", "2020-11-19", "21:00", "충북 청주시 복대동 충북체육관"));
-        datas.add(new Datainfo("축구 초보들만~", "2020-11-20", "17:30", "충북 청주시 개신동 충북운동장"));
+        datas.add(new Datainfo("재밌게 축구할 사람~", "2020-11-18", "20:30", "충북 청주시 개신동 충북대학교", "1/4명"));
+        datas.add(new Datainfo("축구 고수들만 모이세요", "2020-11-19", "21:00", "충북 청주시 복대동 충북체육관","1/6명"));
+        datas.add(new Datainfo("축구 초보들만~", "2020-11-20", "17:30", "충북 청주시 개신동 충북운동장","5/8명"));
 
         listview = (ListView) findViewById(android.R.id.list);
 
